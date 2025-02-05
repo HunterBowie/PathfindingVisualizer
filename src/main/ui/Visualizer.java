c3811773ca86f74650eb8e292bf5c7fa8d89d692
Hunter Bowie <axolotl@students.cs.ubc.ca>
2025-02-05 14:22:15 -0800

    Added specification for Visualizer

---
+++ src/main/ui/Visualizer.java
@@ -0,0 +1,56 @@
+package ui;
+
+import model.Position;
+
+public class Visualizer {
+    
+    // EFFECTS: creates a new Visualizer
+    public Visualizer() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes user input
+    private void runVisualizer() {
+
+    }
+
+    // EFFECTS: gets input from the user and returns it
+    private String getInput() {
+        // CMDS: q: quit, a: add wall, c: clear walls, s: set start pos, e: set end pos
+        return "";
+    }
+
+    // EFFECTS: render the graph by printing it to console
+    private void renderGraph() {
+
+    }
+
+    // REQUIRES: pos must be within bounds of visualizer
+    // MODIFIES: this
+    // EFFECTS: adds a wall to visualizer at given pos
+    private void addWall(Position pos) {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: clears all the walls in the visualizer
+    private void clearWalls() {
+
+    }
+    
+    // REQUIRES: pos must be within bounds of visualizer
+    // MODIFIES: this
+    // EFFECTS: sets the start position to the given pos
+    private void setStartPos(Position pos) {
+
+    }
+
+    // REQUIRES: pos must be within bounds of visualizer
+    // MODIFIES: this
+    // EFFECTS: sets the end position to the given pos
+    private void setEndPos(Position pos) {
+
+    }
+}
+
