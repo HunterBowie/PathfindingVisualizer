3c21c648dab5c6b915a99cf091f016ba847e2ab8
Hunter Bowie <axolotl@students.cs.ubc.ca>
2025-02-08 09:24:04 -0800

    Added implentation for Main

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -2,15 +2,6 @@ package ui;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("0 0 0 0 0 0 0 0 0 0");
-        System.out.println("0 0 0 0 0 0 0 0 e 0");
-        System.out.println("0 0 0 0 0 0 0 2 1 0");
-        System.out.println("0 0 0 0 0 0 2 1 2 0");
-        System.out.println("0 0 0 0 0 2 1 2 0 0");
-        System.out.println("0 0 0 0 2 1 2 0 0 0");
-        System.out.println("0 0 0 2 1 2 0 0 0 0");
-        System.out.println("0 0 2 1 2 0 0 0 0 0");
-        System.out.println("0 0 s 2 0 0 0 0 0 0");
-        System.out.println("0 0 0 0 0 0 0 0 0 0");
+        new Visualizer();
     }
 }
