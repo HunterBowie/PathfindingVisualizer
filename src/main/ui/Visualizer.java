eacfe951a02c3cf6332c2c5fc7763595d7c273f9
Hunter Bowie <axolotl@students.cs.ubc.ca>
2025-02-08 09:24:31 -0800

    Added implementation for Visualizer

--- src/main/ui/Visualizer.java
+++ src/main/ui/Visualizer.java
@@ -1,56 +1,92 @@
 package ui;
 
+import java.util.Scanner;
+
+import model.Graph;
+import model.NodeType;
 import model.Position;
 
 public class Visualizer {
-    
+
+    private Graph graph;
+    private Scanner input;
+
     // EFFECTS: creates a new Visualizer
     public Visualizer() {
-
+        graph = new Graph(10, 10, new Position(0, 0), new Position(9, 9));
+        input = new Scanner(System.in);
+        runVisualizer();
     }
 
     // MODIFIES: this
     // EFFECTS: processes user input
     private void runVisualizer() {
+        boolean running = true;
+        String command = null;
+
+        renderGraph();
+
+        while (running) {
+            command = getCommandInput();
+
+            if (command.equals("q")) {
+                running = false;
+            } else {
+                processCommand(command);
+            }
+            renderGraph();
+        }
 
     }
 
+    // MODIFIES: this
+    // EFFECTS: processes user command
+    private void processCommand(String command) {
+        if (command.equals("a")) {
+            Position pos = getPosInput();
+            graph.setNode(pos, NodeType.WALL);
+        } else if (command.equals("c")) {
+            graph.setAllNodes(NodeType.EMPTY);
+        } else if (command.equals("s")) {
+            Position pos = getPosInput();
+            graph.setStartPos(pos);
+        } else if (command.equals("e")) {
+            Position pos = getPosInput();
+            graph.setEndPos(pos);
+        } else {
+            System.out.println("Please enter valid command :v");
+        }
+    }
+
     // EFFECTS: gets input from the user and returns it
-    private String getInput() {
-        // CMDS: q: quit, a: add wall, c: clear walls, s: set start pos, e: set end pos
-        return "";
+    private String getCommandInput() {
+        System.out.println("CMDS: q: quit, a: add wall, c: clear walls, s: set start pos, e: set end pos");
+        return input.next();
+    }
+
+    // EFFECTS: gets input from the user and returns it
+    private Position getPosInput() {
+        System.out.println("Enter a pos in the form \"row,col\"");
+        String[] raw = input.next().split(",");
+        return new Position(Integer.valueOf(raw[0]), Integer.valueOf(raw[1]));
     }
 
     // EFFECTS: render the graph by printing it to console
     private void renderGraph() {
-
+        for (int row = 0; row < graph.getRows(); row++) {
+            String printRow = "";
+            for (int col = 0; col < graph.getCols(); col++) {
+                Position pos = new Position(row, col);
+                String printChar = String.valueOf(graph.getNode(pos).ordinal());
+                if (pos.equals(graph.getStartPos())) {
+                    printChar = "s";
+                } else if (pos.equals(graph.getEndPos())) {
+                    printChar = "e";
+                }
+                printRow = printRow + printChar;
+            }
+            System.out.println(printRow);
+        }
     }
 
-    // REQUIRES: pos must be within bounds of visualizer
-    // MODIFIES: this
-    // EFFECTS: adds a wall to visualizer at given pos
-    private void addWall(Position pos) {
-
-    }
-
-    // MODIFIES: this
-    // EFFECTS: clears all the walls in the visualizer
-    private void clearWalls() {
-
-    }
-    
-    // REQUIRES: pos must be within bounds of visualizer
-    // MODIFIES: this
-    // EFFECTS: sets the start position to the given pos
-    private void setStartPos(Position pos) {
-
-    }
-
-    // REQUIRES: pos must be within bounds of visualizer
-    // MODIFIES: this
-    // EFFECTS: sets the end position to the given pos
-    private void setEndPos(Position pos) {
-
-    }
 }
-
