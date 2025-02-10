215c3c2b22d4e1d3aaac66cec8e8dff51b556e89
Hunter Bowie <axolotl@students.cs.ubc.ca>
2025-02-09 22:12:27 -0800

    Added class level documentation for Main

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,5 +1,8 @@
 package ui;
 
+/**
+ * Repersents the entry point of the program.
+ **/
 public class Main {
     public static void main(String[] args) throws Exception {
         new Visualizer();
