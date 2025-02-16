245a4645c40ea77314fe009efea680760dcc7be3
Hunter Bowie <axolotl@students.cs.ubc.ca>
2025-02-15 18:52:58 -0800

    Added specification for AStar

---
+++ src/main/model/AStar.java
@@ -0,0 +1,19 @@
+package model;
+
+public class AStar implements Algorithm {
+
+    // EFFECTS: create a new AStar with the given graph
+    public AStar(Graph graph) {
+
+    }
+
+    @Override
+    public void step() {
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+    
+}
