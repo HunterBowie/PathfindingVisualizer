ccc698a0b71b6c4a34a7b833edf32982d5aa3375
Hunter Bowie <axolotl@students.cs.ubc.ca>
2025-02-18 18:17:07 -0800

    Added specification for DepthFirst

---
+++ src/main/model/algo/DepthFirst.java
@@ -0,0 +1,18 @@
+package model.algo;
+
+import exceptions.AlgoFinished;
+import exceptions.AlgoOutOfMoves;
+import model.Graph;
+
+public class DepthFirst extends Algorithm {
+
+    // EFFECTS: creates a DepthFirst algorithm with a given graph
+    public DepthFirst(Graph graph) {
+    }
+
+    @Override
+    public void step() throws AlgoFinished, AlgoOutOfMoves {
+        
+    }
+    
+}
