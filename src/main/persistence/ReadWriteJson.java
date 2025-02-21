667031cab51738543662a1706688080cc02e554c
Hunter Bowie <axolotl@students.cs.ubc.ca>
2025-02-21 14:38:35 -0800

    Added specificatoin of ReadWriteJson

---
+++ src/main/persistence/ReadWriteJson.java
@@ -0,0 +1,21 @@
+package persistence;
+
+import java.io.IOException;
+
+import org.json.JSONObject;
+
+/**
+ * Repersents tools for reading and writing JSON to/from files 
+ **/
+public class ReadWriteJson {
+
+    // EFFECTS: returns the json object from given file
+    public static JSONObject readJSON(String file) throws IOException {
+        return null;
+    }
+
+    // EFFECTS: writes the given json object to given file
+    public static void writeJSON(JSONObject data, String file) {
+    }
+
+}
