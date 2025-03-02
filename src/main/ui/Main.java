721f28399a7157fb297d66b68911100a0e7f8192
Hunter Bowie <axolotl@students.cs.ubc.ca>
2025-03-01 21:35:59 -0800

    Updated Visualizer to reflect load/save capability

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,14 +1,10 @@
 package ui;
 
-import persistence.ReadWriteJson;
-
 /**
  * Repersents the entry point of the program.
  **/
 public class Main {
     public static void main(String[] args) throws Exception {
         new Visualizer();
-        // System.out.println(ReadWriteJson.readJson("graph1.json"));
-
     }
 }
--- src/main/ui/Visualizer.java
+++ src/main/ui/Visualizer.java
@@ -1,6 +1,11 @@
 package ui;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.PatternSyntaxException;
+
+import org.json.JSONObject;
 
 import model.Graph;
 import model.Node;
@@ -10,6 +15,8 @@ import model.algo.AStar;
 import model.algo.Algorithm;
 import model.algo.BreadthFirst;
 import model.algo.DepthFirst;
+import persistence.ParseJson;
+import persistence.ReadWriteJson;
 
 /**
  * Repersents the console interface for the visualization of the graph.
@@ -18,20 +25,56 @@ public class Visualizer {
 
     private Graph graph;
     private Algorithm algo;
-    private Scanner input;
+    private Scanner scanner;
 
     // EFFECTS: creates a new Visualizer
-    public Visualizer() throws InterruptedException {
-        graph = new Graph(6, 6, new Position(0, 0),
-                new Position(5, 5));
-        input = new Scanner(System.in);
-        algo = getAlgoInput();
+    public Visualizer() throws InterruptedException, IOException {
+        setUp();
         runVisualizer();
     }
 
     // MODIFIES: this
+    // EFFECTS: set up the visualizer by initializing the graph and algorithm
+    private void setUp() throws IOException {
+        scanner = new Scanner(System.in);
+        System.out.println();
+        String createLoadCommand = getLoadCreateInput();
+        if (createLoadCommand.equals("c")) {
+            createNewGraph();
+
+        } else {
+            System.out.println();
+            String jsonFile = getInput(
+                    "Enter the json file name (with extension)");
+            JSONObject data = ReadWriteJson.readJson(jsonFile);
+            graph = ParseJson.toGraph(data);
+            algo = ParseJson.toAlgorithm(data, graph);
+        }
+    }
+
+    // MODIFIES: this
+    // EFFECTS: creates a new graph based on user input
+    private void createNewGraph() {
+        System.out.println();
+        try {
+            String graphInput = getInput(
+                    "Enter graph data in the form \"rows,columns\"");
+            String[] splitInput = graphInput.split(",");
+            int rows = Integer.valueOf(splitInput[0]);
+            int cols = Integer.valueOf(splitInput[1]);
+            graph = new Graph(rows, cols, new Position(0, 0), new Position(rows - 1, cols - 1));
+            algo = getAlgoInput();
+
+        } catch (NumberFormatException | PatternSyntaxException | ArrayIndexOutOfBoundsException e) {
+            printInvalidMessage();
+            createNewGraph();
+        }
+
+    }
+
+    // MODIFIES: this
     // EFFECTS: processes user input
-    private void runVisualizer() throws InterruptedException {
+    private void runVisualizer() throws InterruptedException, FileNotFoundException {
         boolean running = true;
         String command = null;
 
@@ -42,12 +85,33 @@ public class Visualizer {
             command = getCommandInput();
 
             if (command.equals("q")) {
+                System.out.println();
+                quit();
+                System.out.println();
                 running = false;
             } else {
                 processCommand(command);
             }
         }
 
+    }
+
+    // EFFECTS: saves or does not save graph based on user input
+    private void quit() throws FileNotFoundException {
+        String saveFile = getInput("Would you like to save your data to a file? (y/n)");
+        if (saveFile.equals("y")) {
+            System.out.println();
+            String fileName = getInput("Enter the name of the file the data will be saved in (with extension)");
+            JSONObject data = ParseJson.fromGraphAlgo(graph, algo);
+            ReadWriteJson.writeJson(data, fileName);
+
+        } else if (saveFile.equals("n")) {
+            // pass
+
+        } else {
+            printInvalidMessage(saveFile);
+            quit();
+        }
     }
 
     // MODIFIES: this
@@ -67,7 +131,7 @@ public class Visualizer {
         } else if (command.equals("r")) {
             runAlgorithm();
         } else {
-            System.out.println("Input '" + command + "' is not valid");
+            printInvalidMessage(command);
         }
     }
 
@@ -83,36 +147,81 @@ public class Visualizer {
             renderGraph();
             Thread.sleep(delay);
         }
-        graph.setAllNodes(NodeType.EMPTY);
+        for (int row = 0; row < graph.getRows(); row++) {
+            for (int col = 0; col < graph.getCols(); col++) {
+                Node node = graph.getNode(new Position(row, col));
+                if (node.getNodeType() != NodeType.WALL) {
+                    node.setNodeType(NodeType.EMPTY);
+                }
+            }
+        }
+        System.out.println();
         algo = getAlgoInput();
     }
 
     // EFFECTS: returns user input after stripped and making lower case
     private String getInput(String prompt) {
         System.out.println(prompt);
-        return input.next().strip().toLowerCase();
+        return scanner.next().strip().toLowerCase();
     }
 
     // EFFECTS: returns input from the user
     private String getCommandInput() {
         return getInput("""
-                Enter one of the following cmds: \n[q] quit \n[a] add wall
-                [c] clear walls \n[s] set start pos \n[e] set end pos
+                Enter one of the following commands:
+                [q] quit
+                [a] add wall
+                [c] clear walls
+                [s] set start pos
+                [e] set end pos
                 [r] run algorithm""");
     }
 
     // EFFECTS: returns position input from the user
     private Position getPosInput() {
-        String input = getInput("Enter a position in the form of \"row,col\"");
-        String[] splitInput = input.split(",");
-        return new Position(Integer.valueOf(splitInput[0]), Integer.valueOf(splitInput[1]));
+        try {
+            String input = getInput("Enter a position in the form \"row,column\"");
+            String[] splitInput = input.split(",");
+            return new Position(Integer.valueOf(splitInput[0]), Integer.valueOf(splitInput[1]));
+        } catch (NumberFormatException | PatternSyntaxException | ArrayIndexOutOfBoundsException e) {
+            printInvalidMessage();
+            System.out.println();
+            return getPosInput();
+        }
+    }
+
+    // EFFECTS: prints a generic invalid message with given invalidInput
+    private void printInvalidMessage(String invalidInput) {
+        System.out.println("Input '" + invalidInput + "' is not valid");
+    }
+
+    // EFFECTS: prints a generic invalid message
+    private void printInvalidMessage() {
+        System.out.println("Input is not valid");
+    }
+
+    // EFFECTS: returns either "c" or "l" from user input command
+    private String getLoadCreateInput() {
+        String input = getInput("""
+                Enter one of the following commands:
+                [c] create new graph
+                [l] load graph from file""");
+
+        if (input.equals("c") || input.equals("l")) {
+            return input;
+        }
+        printInvalidMessage(input);
+        System.out.println();
+        return getLoadCreateInput();
     }
 
     // EFFECTS: return algorithm input from the user
     private Algorithm getAlgoInput() {
         String input = getInput("""
                 Enter a type of pathfinding algorithm:
-                [a] A* \n[d] depth first \n[b] breadth first""");
+                [a] A*
+                [d] depth first
+                [b] breadth first""");
 
         if (input.equals("a")) {
             return new AStar(graph);
@@ -124,12 +233,13 @@ public class Visualizer {
             return new DepthFirst(graph);
         }
 
-        System.out.println("Input '" + input + "' is not valid");
+        printInvalidMessage(input);
 
         return getAlgoInput();
 
     }
 
+    // REQUIRES: this.setUp() must be called
     // EFFECTS: render the graph by printing it to console
     private void renderGraph() {
         for (int row = 0; row < graph.getRows(); row++) {
