b0efcc7eb689e8dd7132c0f7828f63a8f65518f5
Hunter Bowie <axolotl@students.cs.ubc.ca>
2025-03-09 15:37:06 -0700

    Added implementation for SwingGUI

--- src/main/ui/SwingGUI.java
+++ src/main/ui/SwingGUI.java
@@ -1,7 +1,18 @@
 package ui;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
+import javax.swing.Timer;
+
+import exceptions.AlgoFinished;
+import exceptions.AlgoOutOfMoves;
 import model.Graph;
+import model.Position;
+import model.algo.AStar;
 import model.algo.Algorithm;
 
 /**
@@ -9,40 +20,82 @@ import model.algo.Algorithm;
  */
 public class SwingGUI extends JFrame {
 
+    private static final int UPDATE_DELAY_MS = 10;
+
+    private GraphPanel graphPanel;
+    private Algorithm algo;
+    private boolean running = false;
+
     // EFFECTS: constructs a new SwingGUI
     public SwingGUI() {
+        super("Pathfinding Visualization");
+        graphPanel = new GraphPanel(new Graph(30, 30,
+                new Position(5, 5), new Position(20, 20)));
+        algo = new AStar(graphPanel.getGraph());
+        add(graphPanel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setUndecorated(false);
+        setVisible(true);
+        pack();
+        centreOnScreen();
+        start();
+        setResizable(false);
+        addKeyListener(new KeyInput(this));
+        addMouseListener(new MouseInput(this));
     }
-    
+
     // MODIFIES: this
     // EFFECTS: initializes a timer that updates the visualization
     private void start() {
+        Timer t = new Timer(UPDATE_DELAY_MS, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                graphPanel.repaint();
+                if (running) {
+                    try {
+                        algo.step();
+                    } catch (AlgoOutOfMoves e) {
+                        running = false;
+                    } catch (AlgoFinished e) {
+                        running = false;
+                    }
+                }
+            }
+        });
+
+        t.start();
     }
 
     // MODIFIES: this
     // EFFECTS: location of frame is set so frame is centred on screen
     private void centreOnScreen() {
+        Dimension scrn = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation((scrn.width - getWidth()) / 2, (scrn.height - getHeight()) / 2);
     }
 
     // getters and setters
     public Algorithm getAlgorithm() {
-        return null;
+        return algo;
     }
 
     public void setAlgorithm(Algorithm algo) {
+        this.algo = algo;
     }
 
     public Graph getGraph() {
-        return null;
+        return graphPanel.getGraph();
     }
 
     public void setGraph(Graph graph) {
+        graphPanel.setGraph(graph);
     }
 
     public boolean getRunning() {
-        return false;
+        return running;
     }
 
     public void setRunning(boolean running) {
+        this.running = running;
     }
 
 }
